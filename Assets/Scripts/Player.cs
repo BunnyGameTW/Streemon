3266dec5666cd3698476ed02981a7f23d8a1f8f4
unknown <c6150261602@yahoo.com.tw>
2017-09-06 00:36:49 +0800

    0906 add start scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Vector3 mouseScrPos = Vector3.zero;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePoint = Input.mousePosition;
+             mouseScrPos = Camera.main.ScreenToWorldPoint(mousePoint);
+            mouseScrPos.z = 0.0f;
+            Debug.Log(mouseScrPos);
+            float distanceX = mouseScrPos.x - transform.position.x;//offsetX
+           
+                                            // robotPoint = gameObject.transform.position;
+                                            //  lastPlayerPos = transform.position;
+                                            //  move(distanceX, 0);
+        }
+        move(1, 1);
+
+
+    }
+    void move(float x,float y) {
+       // transform.position = Vector3.Lerp(transform.position, mouseScrPos, 1.5f * Time.deltaTime);
+       if(transform.position )
+       // transform.position += new Vector3(x, y, 0) * Time.deltaTime;//speed will change by clickPoint
+
+    }
+}
