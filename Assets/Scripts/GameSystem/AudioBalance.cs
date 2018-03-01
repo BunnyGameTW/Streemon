182bef081550cd9c3349e8be21516c740ddc8845
Angel Cheng <c6150261602@yahoo.com.tw>
2018-03-01 22:59:37 +0800

    add bgm balancer

---
+++ Assets/Scripts/GameSystem/AudioBalance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioBalance : MonoBehaviour {
+    public AudioMixer auMixer;
+    public float vloumn;
+	// Use this for initialization
+	void Start () {
+        auMixer.SetFloat("volume", vloumn);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        auMixer.SetFloat("volume", vloumn);
+    
+    }
+}
--- Assets/Scripts/GameSystem/SoundManager.cs
+++ Assets/Scripts/GameSystem/SoundManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
 public class SoundManager : MonoBehaviour {
     [Serializable]
     public struct music
@@ -27,6 +27,8 @@ public class SoundManager : MonoBehaviour {
     public music[] musics;
     AudioSource bgmSource, seSource;
     public static SoundManager sound;//singleton pattern
+    public AudioMixerGroup auGroup;
+    public float mixerVol;
     // Use this for initialization
     private void Awake()
     {
@@ -37,23 +39,38 @@ public class SoundManager : MonoBehaviour {
         if(SceneManager.GetActiveScene().name =="SblueRoom") seSource = GameManager.game.Player.GetComponent<AudioSource>();
     }
     void Start () {
-        if (SceneManager.GetActiveScene().name != "Sout") seSource = GameManager.game.Player.GetComponent<AudioSource>();
-        for (int i = 0; i < musics.Length; i++)
+        if (SceneManager.GetActiveScene().name != "Sout" && SceneManager.GetActiveScene().name != "Stitle") seSource = GameManager.game.Player.GetComponent<AudioSource>();//get player se
+        for (int i = 0; i < musics.Length; i++)//set bgm
         {
             if (SceneManager.GetActiveScene().name == musics[i].sceneName)
             {
                 bgmSource = gameObject.AddComponent<AudioSource>();
+                bgmSource.outputAudioMixerGroup = auGroup;
                 bgmSource.clip = musics[i].bgm;               
                 bgmSource.loop = true;
                 bgmSource.Play();
             }
         }
-       
-	}
+        if (SceneManager.GetActiveScene().name == "Sopen")//TODO:調整數值
+        {
+            mixerVol = 20;         
+        }
+        else
+        {
+            mixerVol = 0;
+        }
+        auGroup.audioMixer.SetFloat("volume", mixerVol);
+    }
     public void playOne(AudioClip se) {
         seSource.PlayOneShot(se);
     }
     public void stopSE() {
         seSource.Stop();
+    }
+    public void bgmFadeIn()
+    {
+        //-10~0 10~20
+        auGroup.audioMixer.SetFloat("volume", mixerVol);
+
     }
 }
